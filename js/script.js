d25b15e3dffb5b40729da190733cf98ed5d9d4a7
greentreegroup <isaacandrewrose@greentree.group>
2023-07-03 16:48:34 +0100

    mobile end issue fixed

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 /*-----Javascript for Project Name */
-$(document).ready(function() {
-   // Fakes the loading setting a timeout
-   setTimeout(function() {
-      $('body').addClass('loaded');
-   }, 1500);
+$(document).ready(function () {
+  // Fakes the loading setting a timeout
+  setTimeout(function () {
+    $("body").addClass("loaded");
+  }, 1500);
 });
 
-const navbar = document.getElementById('navbar');
-   let scrolled = false;
+const navbar = document.getElementById("navbar");
+let scrolled = false;
 
-   window.onscroll = function () {
-      if (window.pageYOffset > 500) {
-         navbar.classList.remove('top');
-         navbar.classList.add('bg-light-new');
-         if (!scrolled) {
+window.onscroll = function () {
+  if (window.pageYOffset > 500) {
+    navbar.classList.remove("top");
+    navbar.classList.add("bg-light-new");
+    if (!scrolled) {
+    }
+    setTimeout(function () {
+      scrolled = true;
+    }, 250);
+  } else {
+    navbar.classList.remove("bg-light-new");
+    scrolled = false;
+  }
 
-         }
-         setTimeout(function () {
-
-            scrolled = true;
-         }, 250);
-      } else {
-         navbar.classList.remove('bg-light-new');
-         scrolled = false;
-      }
-   }
+}
